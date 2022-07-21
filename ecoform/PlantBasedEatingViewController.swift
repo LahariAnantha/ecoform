7472036f8d74ce4f5e7b3e2306bdb1131675ee41
calistacr <calistacruikshank@gmail.com>
2022-07-21 15:54:29 -0700

    there's an issue on the plant based eating boxes but idk why

--- ecoform/PlantBasedEatingViewController.swift
+++ ecoform/PlantBasedEatingViewController.swift
@@ -9,21 +9,124 @@ import UIKit
 
 class PlantBasedEatingViewController: UIViewController {
 
+    //emojis
+    
+    @IBOutlet weak var firstEmoji: UILabel!
+    
+    @IBOutlet weak var secondEmoji: UILabel!
+    
+    @IBOutlet weak var thirdEmoji: UILabel!
+    
+    @IBOutlet weak var fourthEmoji: UILabel!
+    
+    @IBOutlet weak var fifthEmoji: UILabel!
+    
+    @IBOutlet weak var sixthEmoji: UILabel!
+    
+    
+    //boxes
+    
+    @IBOutlet weak var buttonColorOne: UIButton!
+    
+    @IBOutlet weak var buttonColorTwo: UIButton!
+    
+    @IBOutlet weak var buttonColorThree: UIButton!
+    
+    @IBOutlet weak var buttonColorFour: UIButton!
+    
+    @IBOutlet weak var buttonColorFive: UIButton!
+    
+    @IBOutlet weak var buttonColorSix: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        firstEmoji.isHidden = true
+        secondEmoji.isHidden = true
+        thirdEmoji.isHidden = true
+        fourthEmoji.isHidden = true
+        fifthEmoji.isHidden = true
+        sixthEmoji.isHidden = true
+        buttonColorOne.tintColor = UIColor(named: "initialColor")
+        buttonColorTwo.tintColor = UIColor(named: "initialColor")
+        buttonColorThree.tintColor = UIColor(named: "initialColor")
+        buttonColorFour.tintColor = UIColor(named: "initialColor")
+        buttonColorFive.tintColor = UIColor(named: "initialColor")
+        buttonColorSix.tintColor = UIColor(named: "initialColor")
+        
         // Do any additional setup after loading the view.
     }
     
+    
+//button color change + emoji appearance
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func firstBox(_ sender: UIButton) {
+    if firstEmoji.isHidden == true {
+            firstEmoji.isHidden = false
+            buttonColorOne.tintColor = UIColor(named: "clickedColor")
+    } else {
+            firstEmoji.isHidden = true
+        buttonColorOne.tintColor = UIColor(named: "initialColor")
+        }
     }
-    */
+    
 
+    @IBAction func secondBox(_ sender: UIButton) {
+    if secondEmoji.isHidden == true {
+            secondEmoji.isHidden = false
+            buttonColorTwo.tintColor = UIColor(named: "clickedColor")
+    } else {
+            secondEmoji.isHidden = true
+        buttonColorTwo.tintColor = UIColor(named: "initialColor")
+        }
+    }
+    
+
+    @IBAction func thirdBox(_ sender: UIButton) {
+    if thirdEmoji.isHidden == true {
+            thirdEmoji.isHidden = false
+            buttonColorThree.tintColor = UIColor(named: "clickedColor")
+    } else {
+            thirdEmoji.isHidden = true
+        buttonColorThree.tintColor = UIColor(named: "initialColor")
+        }
+    }
+    
+
+    
+   
+    @IBAction func fourthBox(_ sender: UIButton) {
+    if fourthEmoji.isHidden == true {
+            fourthEmoji.isHidden = false
+            buttonColorFour.tintColor = UIColor(named: "clickedColor")
+    } else {
+            fourthEmoji.isHidden = true
+        buttonColorFour.tintColor = UIColor(named: "initialColor")
+        }
+    }
+    
+
+   
+    @IBAction func fifthBox(_ sender: UIButton) {
+    if fifthEmoji.isHidden == false {
+            buttonColorFive.tintColor = UIColor(named: "clickedColor")
+    } else {
+            fifthEmoji.isHidden = true
+        buttonColorFive.tintColor = UIColor(named: "initialColor")
+        }
+    }
+
+
+    @IBAction func sixthBox(_ sender: Any) {
+    if sixthEmoji.isHidden == true {
+            sixthEmoji.isHidden = false
+            buttonColorSix.tintColor = UIColor(named: "clickedColor")
+    } else {
+            sixthEmoji.isHidden = true
+        buttonColorSix.tintColor = UIColor(named: "initialColor")
+        }
+    }
+    
 }
